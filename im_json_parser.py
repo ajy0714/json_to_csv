18a95f657596c8b13f9cb87f940cfdd9d12742da
Jun-Young Ahn <swh01007@gmail.com>
2022-03-23 01:44:44 +0900

    layout bug fixed, depth 오류 수정 필요
    layout bug fixed
    key 값을 전달할 때 가장 최하단의 key 값을 넣어야하지만 그렇지 않은 경우 버그 발생
    (예. JSON depth가 3인데 key 값을 2개만 넣으면 오류 발생)

--- im_json_parser.py
+++ im_json_parser.py
@@ -1,9 +1,9 @@
-import sys, json
-import pandas as pd
+import json
+import sys
 import time
-from PyQt5.QtWidgets import QLineEdit, QPlainTextEdit, QPushButton, QTableWidget, QVBoxLayout, QHeaderView, \
-    QApplication, QTableWidgetItem, QWidget
-
+import pandas as pd
+from PyQt5.QtWidgets import QLineEdit, QPlainTextEdit, QPushButton, QTableWidget, QVBoxLayout, QApplication, QTableWidgetItem, QWidget
+from PyQt5.QtCore import Qt
 
 class MyApp(QWidget):
 
@@ -16,31 +16,29 @@ class MyApp(QWidget):
         self.setGeometry(1000, 50, 800, 800)
 
         self.key_list = QLineEdit(self)
-        self.key_list.setText('output_object result')
+        self.key_list.setPlaceholderText('추출하고자 값의 키를 공백으로 구분하여 입력하세요(ex. output_object result IM)')
         self.json_content = QPlainTextEdit(self)
+        self.json_content.setPlaceholderText('JSON의 내용을 입력하세요')
 
-        self.btn1 = QPushButton('step1. json 변환', self)
+        self.btn1 = QPushButton('step1. Convert into JSON', self)
         self.btn1.clicked.connect(self.button_event)
 
-        self.btn2 = QPushButton('step2. tsv로 저장', self)
+        self.btn2 = QPushButton('step2. Save as TSV file', self)
         self.btn2.clicked.connect(self.save_into_tsv)
 
         self.tableWidget = QTableWidget(self)
         self.tableWidget.resize(300, 300)
-        # self.tableWidget.setSortingEnabled(True)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.key_list)
         layout.addWidget(self.json_content)
+        layout.addWidget(self.key_list)
         layout.addWidget(self.tableWidget)
         layout.addWidget(self.btn1)
         layout.addWidget(self.btn2)
-
         self.setLayout(layout)
         self.show()
 
     def button_event(self):
-
         try:
             key_list = self.key_list.text().split(' ')
             json_content = json.loads(self.json_content.toPlainText())
@@ -48,21 +46,21 @@ class MyApp(QWidget):
             for i in key_list:
                 json_content = json_content.get(i)
 
-            df = pd.DataFrame(json_content)
+            self.df = pd.DataFrame(json_content)
+            print(self.df)
 
             self.tableWidget.setRowCount(len(json_content))
-            self.tableWidget.setColumnCount(len(df.columns))
-            self.tableWidget.setHorizontalHeaderLabels(df.columns)
-            header = self.tableWidget.horizontalHeader()
+            self.tableWidget.setColumnCount(len(self.df.columns))
+            self.tableWidget.setHorizontalHeaderLabels(self.df.columns)
 
             for i in range(0, len(json_content)):
-                header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
-                for j in range(0, len(df.columns)):
-                    temp = str(df.iloc[i, j])
-                    self.tableWidget.setItem(i, j, QTableWidgetItem(temp))
-                    # self.tableWidget.item(i, j).setTextAlignment()
+                for j in range(0, len(self.df.columns)):
+                    self.tableWidget.setItem(i, j, QTableWidgetItem(str(self.df.iloc[i, j])))
+                    self.tableWidget.item(i, j).setTextAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
 
-            self.df = df
+            for i in range(0, len(self.df.columns)):
+                self.tableWidget.resizeColumnToContents(i)
+
         except Exception as e:
             print('error occurred: ', e)
 
@@ -71,7 +69,6 @@ class MyApp(QWidget):
         file_name = "imCalcResult_" + str(tm.tm_year) + str(tm.tm_mon) + str(tm.tm_mday) + str(tm.tm_hour) + str(
             tm.tm_min) + str(tm.tm_sec) + '.tsv'
         self.df.to_csv(file_name, sep='\t')
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
